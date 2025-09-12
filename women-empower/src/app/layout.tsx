612e51676b77b54d314b9c3967dbe28f5198b090
MaheshBahirSkramby <mahesh.bahir@skramby.com>
2025-09-12 18:15:21 +0530

    create NavBar & Footer

--- women-empower/src/app/layout.tsx
+++ women-empower/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+//import "./globals.css";
+import NavBar from "./component/ui/utlity/NavBar";
+import Footer from "./component/ui/utlity/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,15 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-/*
-navbar
- */
-
+      <NavBar />
         {children}
-/*
-
-footer
- */
+      <Footer />
       </body>
     </html>
   );
