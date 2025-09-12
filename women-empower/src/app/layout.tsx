9e936a27cba13690c2cbc18fac5c753d7842b3c0
Vishal-Lodhe-Skramby <vishallodhe.skramby@gmail.com>
2025-09-12 19:13:23 +0530

    remove bg

--- women-empower/src/app/layout.tsx
+++ women-empower/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-//import "./globals.css";
+import "./globals.css";
 import NavBar from "./component/ui/utlity/NavBar";
 import Footer from "./component/ui/utlity/Footer";
 
