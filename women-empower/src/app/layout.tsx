b607e4e04e0cbcb04ea9cbc121f8162d6e6606a5
Vishal-Lodhe-Skramby <vishallodhe.skramby@gmail.com>
2025-10-10 01:35:28 +0530

    update

--- women-empower/src/app/layout.tsx
+++ women-empower/src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
             {children}
           </DashboardLayout>
         ) : (
-          // User Layout
+
           <>
             <NavBar />
             <ScrollToTopButton />
