f7ac42d2912dc20e51ac6ec05f1fd463cca2c87f
rushilodhe002 <rushilodhe002@gmail.com>
2026-03-26 10:52:07 +0530

    next.config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Skip ESLint during production builds to avoid fail-on-warn
+  output: 'export',   // VERY IMPORTANT
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Keep TS type checking; flip to true only if you want to bypass TS errors
+
   typescript: {
     ignoreBuildErrors: false,
   },
+
   images: {
+    unoptimized: true,   // Required for static export
     remotePatterns: [
       {
         protocol: 'https',
